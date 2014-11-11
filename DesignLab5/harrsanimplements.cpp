d5b8c9325636664d95b2dd45c22f5597b60e86c6
Harrsan <Harrsan@10.16.124.96>
2014-11-11 15:11:44 -0500

    Comments added final

--- DesignLab5/harrsanimplements.cpp
+++ DesignLab5/harrsanimplements.cpp
@@ -18,7 +18,7 @@ using namespace std;
 //The constructor for patient class which initializes the values for condition as "UNKNOWN",
 //the first and last name as blanks, the healthcard number, bed number, contraction rate,
 //and cervix dilation as 0.
-patient::patient()
+patient::patient()//sets patient data to start up values to be determined after function started
 {
 	condition= UNKNOWN;
 	first_name="";
@@ -30,11 +30,11 @@ patient::patient()
 	cervix_dilation=0;
 
 }
-patient::~patient()
+patient::~patient()//destructor for patient class
 {
 
 }
-void patient::resetpatient(){
+void patient::resetpatient(){//clears all patient data
 	condition= UNKNOWN;
 	first_name="";
 	last_name="";
@@ -44,7 +44,7 @@ void patient::resetpatient(){
 	contraction_rate=7;
 	cervix_dilation=0;
 }
-void patient::printstatus(){ //function that does the switch logic autmatically
+void patient::printstatus(){ //function that prints the condition based on the enumerated value
 	switch(condition){
 	case UNKNOWN:
 		cout<<"UNKNOWN";
@@ -81,28 +81,29 @@ enum_Status patient::examinepatients()//function for adding patients
 
 		break;
 	case CHECKUP:
-		condition=enum_Status(rand()%2+1);
+		condition=enum_Status(rand()%2+1);//randomly assigns release or labour condition after check up
+
 
 		break;
-	case ADMIT:
+	case ADMIT://case prints Admission to birthing unit where men become fathers
 		cout<<"ADMITTED TO BIRTHING UNIT\n";
 		break;
 	case LABOUR:
-		cout<<"Input cervix dilation and contraction rate\n";
+		cout<<"Input cervix dilation and contraction rate\n";//prompts and receives current cervix dilation
 		cin >>cervix_dilation;
 		cin >> contraction_rate;
-		if (cervix_dilation >=4 && contraction_rate< 4)
+		if (cervix_dilation >=4 && contraction_rate< 4)//determines whether or not needs to be admitted
 		condition=ADMIT;
 
 	}
-	return temp;
+	return temp;//returns status
 }
 
 
 void patient::showpatients()
 {
 	cout<<setw(20)<<first_name<<setw(20)<<last_name<<setw(20)<<healthcard_number<<setw(20)<<bed_number<<setw(20)<<contraction_rate<<setw(20)<<cervix_dilation<<setw(20);
-
+	//prints patient information in one line
 }
 
 
